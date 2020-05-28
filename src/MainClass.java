ae64338fed179854e1c2a6a9765429288da4acfd
mustafaag <mustafaaga23@gmail.com>
2020-05-28 15:45:26 +0200

    Megabyte Converter

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,9 @@
 public class MainClass {
 	
 	public static void  main(String[] args) {
-		System.out.println("Hello world!");
-		SpeedConverter.printConversion(1.5);
-		SpeedConverter.printConversion(95.75);
-		SpeedConverter.printConversion(-5.6);
-		SpeedConverter.printConversion(0);
+		MegaByteConverter.printMegaBytesAndKiloBytes(2500);
+		MegaByteConverter.printMegaBytesAndKiloBytes(-1024);
+		MegaByteConverter.printMegaBytesAndKiloBytes(5000);	
 	}
 	
 }
--- src/MegaByteConverter.java
+++ src/MegaByteConverter.java
@@ -1,4 +1,14 @@
 
 public class MegaByteConverter {
-
+	
+	public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+		if(kiloBytes <0 ) {
+			System.out.println("Invalid Value");
+		}else {
+			String YY = String.valueOf(kiloBytes / 1024);
+			String ZZ = String.valueOf(kiloBytes % 1024);
+			
+			System.out.println(String.valueOf(kiloBytes)+"KB = " + YY + " MB and " +ZZ  +" KB");
+		}
+	}
 }
